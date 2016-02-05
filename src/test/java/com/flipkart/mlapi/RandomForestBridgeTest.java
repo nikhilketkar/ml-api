ed6b321bc1bab00688ab611b3e053e34890aefc8
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-02-05 15:02:22 +0530

    RF coding complete

---
+++ src/test/java/com/flipkart/mlapi/RandomForestBridgeTest.java
@@ -0,0 +1,12 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class RandomForestBridgeTest extends TestCase {
+
+    public void testRandomForestBridge()
+    {
+        assertTrue( true );
+    }
+
+}
